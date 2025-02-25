53abd5537805f1cc2b485d8fad95e41b3c8c3e86
mourad <you@example.com>
2025-02-25 12:01:55 -0500

    task 8: pylint

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+""" module to run server """
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,12 +6,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """ Home page """
     return render_template('index.html')
 
 @app.route("/emotionDetector")
 def emotion_detector_request():
+    """ Function to calculate dominant emotion and return it back """
     statement = request.args.get('textToAnalyze')
     emotions = emotion_detector(statement)
-    if emotions["dominant_emotion"] == None:
+    if emotions["dominant_emotion"] is None:
         return 'Invalid text! Please try again!'
-    return f'For the given statement, the system response is {emotions}. The dominant emotion is {emotions["dominant_emotion"]}.'
+    return f'For the given statement, the system response is {emotions}. \
+                The dominant emotion is {emotions["dominant_emotion"]}.'
